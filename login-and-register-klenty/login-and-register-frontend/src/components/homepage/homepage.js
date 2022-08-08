350faf488b1ab4eebc08895f1b7efb291a24c747
pavankalyangojju <95854682+pavankalyangojju@users.noreply.github.com>
2022-08-08 23:48:02 +0530

    front enf back end

--- login-and-register-klenty/login-and-register-frontend/src/components/homepage/homepage.js
+++ login-and-register-klenty/login-and-register-frontend/src/components/homepage/homepage.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "./homepage.css"
-import Comments  from "../comments"
+import Comments  from "../comments/Comments"
 
 const Homepage = ({setLoginUser}) => {
     return (
